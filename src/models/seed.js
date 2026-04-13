789f607f49d5e505d21a0679047d559fc8351662
ajaykk <ajaykk@pinnaclesystems.com>
2026-04-13 09:53:31 +0000

    item seed

--- src/models/seed.js
+++ src/models/seed.js
@@ -8,10 +8,21 @@ import { seedDepartments } from "./seeds/departments.js";
 import { seedEmployees } from "./seeds/employees.js";
 import { seedEmployeeCategories } from "./seeds/employeeCategories.js";
 import { seedFinYears } from "./seeds/finYears.js";
+import { seedGsms } from "./seeds/gsms.js";
+import { seedHsns } from "./seeds/hsns.js";
+import { seedItemGroups } from "./seeds/itemGroups.js";
 import { seedPages } from "./seeds/pages.js";
+import { seedPayTerms } from "./seeds/payTerms.js";
+import { seedParties } from "./seeds/parties.js";
 import { seedRoleOnPages } from "./seeds/roleOnPages.js";
 import { seedRoles } from "./seeds/roles.js";
+import { seedSizeTemplates } from "./seeds/sizeTemplates.js";
 import { seedStates } from "./seeds/states.js";
+import { seedStyleItems } from "./seeds/styleItems.js";
+import { seedTaxTemplateDetails } from "./seeds/taxTemplateDetails.js";
+import { seedTaxTemplates } from "./seeds/taxTemplates.js";
+import { seedTaxTerms } from "./seeds/taxTerms.js";
+import { seedUoms } from "./seeds/uoms.js";
 import { seedUserOnBranches } from "./seeds/userOnBranches.js";
 import { seedUsers } from "./seeds/users.js";
 
@@ -25,11 +36,22 @@ async function main() {
   await seedDepartments();
   await seedEmployeeCategories();
   await seedFinYears();
+  await seedPayTerms();
   await seedRoles();
   await seedRoleOnPages();
+  await seedTaxTerms();
+  await seedTaxTemplates();
+  await seedTaxTemplateDetails();
   await seedEmployees();
   await seedUsers();
   await seedUserOnBranches();
+  await seedParties();
+  await seedUoms();
+  await seedHsns();
+  await seedGsms();
+  await seedSizeTemplates();
+  await seedItemGroups();
+  await seedStyleItems();
 }
 
 main()
