40ad89697e0d3afe3d7799b24d7667a0f3950d19
arun <arunpinnacle10@gmail.com>
2026-07-21 18:52:16 +0530

    Feat: same Machine can't take multiple employee logics implemented

--- src/services/availableMachine.service.js
+++ src/services/availableMachine.service.js
@@ -1,8 +1,19 @@
 import { prisma } from "../lib/prisma.js";
+import { endDate, startDate } from "../utils/helper.js";
+import {  PrismaClient } from "@prisma/client";
 
 async function get(req) {
-  const { departmentId } = req.query;
+  const { departmentId, machineId } = req.query;
   const departmentIdNum = Number(departmentId);
+  const machineNum = Number(machineId);
+
+  if (machineId && isNaN(machineNum)) {
+    return {
+      statusCode: 1,
+      message: "Invalid or missing MachineId",
+      data: [],
+    };
+  }
 
   if (!departmentId || isNaN(departmentIdNum)) {
     return {
@@ -11,39 +22,71 @@ async function get(req) {
       data: [],
     };
   }
-  const date = new Date();
-  const dateOnly = new Date(
-    date.getFullYear(),
-    date.getMonth(),
-    date.getDate(),
-  );
+
+  if (!startDate || !endDate) {
+    return {
+      statusCode: 1,
+      message: "Invalid or missing startDate/endDate",
+      data: [],
+    };
+  }
+
+  const startDateObj = new Date(startDate);
+  const endDateObj = new Date(endDate);
+
+  if (isNaN(startDateObj) || isNaN(endDateObj)) {
+    return {
+      statusCode: 1,
+      message: "startDate/endDate must be valid dates",
+      data: [],
+    };
+  }
+
+  const M_WHERE = {
+    stDatetime: {
+      gte: startDateObj,
+      lt: endDateObj,
+    },
+    isAvailable: false,
+  };
+
+  if (machineId) {
+    M_WHERE.Machineid = {
+      not: machineNum,
+    };
+  }
 
   const busyMachines = await prisma?.takenmachines?.findMany({
-    where: {
-      stDate: dateOnly,
-      isAvailable: false,
-    },
+    where: M_WHERE,
     select: {
-      id: true,
+      Machineid: true,
+      User: true,
+      JobCard: true,
+      ProcessRoute: true,
     },
-  });
-  var busyMachinesArray = busyMachines?.map((mdata) => mdata?.id) || [];
-  const availableMachine = await prisma?.machine?.findMany({
-    where: {
-      AND: [
-        { id: { notIn: busyMachinesArray } },
-        { departmentId: departmentIdNum },
-      ],
-    },
-    select:{
-      id:true,
-      name:true
-    }
   });
 
+  const availableMachine = await prisma?.machine?.findMany({
+    where: { departmentId: departmentIdNum },
+    select: {
+      id: true,
+      name: true,
+    },
+  }) || [];
+
+  const finalizeMachine = availableMachine.map((element) => {
+    const busyMachine_filter = busyMachines?.find(
+      (bmf) => bmf.Machineid === element?.id,
+    );
+    if (busyMachine_filter) {
+      return { ...element, busy: true, busy_by: busyMachine_filter?.User,JobCard:busyMachine_filter?.JobCard };
+    }
+    return { ...element, busy: false };
+  });
+ 
   return {
     statusCode: 0,
-    data: { machines: availableMachine },
+    data: { machines: finalizeMachine },
   };
 }
 
